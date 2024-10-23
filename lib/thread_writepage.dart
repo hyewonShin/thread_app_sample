fa5cc9b43c11ef590bb6fdd4a33a69a2a7b99736
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-10-23 17:54:28 +0900

    Initial commit

---
+++ lib/thread_writepage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ThreadWritePage extends StatelessWidget {
+  const ThreadWritePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('새로운 스레드'),
+      ),
+    );
+  }
+}
